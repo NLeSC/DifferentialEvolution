c0ced3e581a14ba127fb07bb3ebce955413d80f7
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2013-07-25 11:17:49 +0200

    statespace model preparations

--- src/nl/esciencecenter/diffevo/statespacemodels/StateSpaceModel.java
+++ src/nl/esciencecenter/diffevo/statespacemodels/StateSpaceModel.java
@@ -3,5 +3,8 @@ package nl.esciencecenter.diffevo.statespacemodels;
 public interface StateSpaceModel {
 	
 	public double calcLogLikelihood(double[] initState, double[] parameterVector, double[][] forcing, double[] priorTimes);
+	
+	public String getName();
+	
 
 }
