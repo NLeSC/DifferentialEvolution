f27c4bb45a205181987bbef4eb54eebeaeec807c
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2013-07-26 16:22:21 +0200

    Model object is now created using a Factory design pattern.

---
+++ src/nl/esciencecenter/diffevo/LinearDynamicStateSpaceModelFactory.java
@@ -0,0 +1,14 @@
+package nl.esciencecenter.diffevo;
+
+
+import nl.esciencecenter.diffevo.statespacemodels.LinearDynamicStateSpaceModel;
+import nl.esciencecenter.diffevo.statespacemodels.Model;
+
+public class LinearDynamicStateSpaceModelFactory implements ModelFactory{
+
+	public Model create(double[] state,double[] parameterVector, double[] forcing, double[] times) {
+		Model model = new LinearDynamicStateSpaceModel(state, parameterVector, forcing, times);
+
+		return model;
+	}
+}
