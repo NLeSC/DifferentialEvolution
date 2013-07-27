a3429ec28226e69ba4ec39debf222c261332df16
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2013-07-27 15:47:23 +0200

    moved to likelihoodfunctions package

--- src/nl/esciencecenter/diffevo/LikelihoodFunctionFactory.java
+++
@@ -1,9 +0,0 @@
-package nl.esciencecenter.diffevo;
-
-import nl.esciencecenter.diffevo.statespacemodels.LikelihoodFunction;
-
-public interface LikelihoodFunctionFactory {
-	
-	public LikelihoodFunction create();
-
-}
