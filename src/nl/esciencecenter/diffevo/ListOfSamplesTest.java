e135e0f56c9170dd0dc67ed37c29a333b7341514
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2013-08-01 14:32:54 +0200

    updated ListOfSamplesTest.java

--- src/nl/esciencecenter/diffevo/ListOfSamplesTest.java
+++ src/nl/esciencecenter/diffevo/ListOfSamplesTest.java
@@ -41,7 +41,8 @@ public class ListOfSamplesTest {
 	@Test
 	public void testAdd() {
 
-		int nPop = 9;
+		// make the list of samples
+		int nPop = 2;
 		int numberOfDimensions = 3;
 		double[] lowerBounds = new double[]{-10,2.0,40};
 		double[] upperBounds = new double[]{102,4.5,40.17};
@@ -49,55 +50,60 @@ public class ListOfSamplesTest {
 		ParSpace parSpace = new ParSpace(lowerBounds,upperBounds,parNames);
 		ListOfSamples listOfSamplesActual = new ListOfSamples(nPop,parSpace);
 		
+		// make the new sample
 		Sample sample = new Sample(numberOfDimensions);
-		int sampleCounterExpected = 9;
-		double[] parameterVectorExpected = new double[]{-3.4,3.2,40.05};
+		int sampleCounterExpected = nPop+1;
 		sample.setSampleCounter(sampleCounterExpected);
+		
+		double[] parameterVectorExpected = new double[]{-3.4,3.2,40.05};
+		sample.setParameterVector(parameterVectorExpected);
 		
 		double objScoreExpected = -1.234;
 		sample.setObjScore(objScoreExpected);
 		
+		// add the new sample to the list of samples
 		listOfSamplesActual.add(sample);
 		
+		// verify the size
 		assertEquals(nPop+1, listOfSamplesActual.getSize());
 		
-		double[] parameterVectorActual = listOfSamplesActual.getParameterVector(nPop+1);
+		// verify the parameter vector 
+		double[] parameterVectorActual = listOfSamplesActual.getParameterVector(nPop);
 		double parameterVectorAcceptableDifference = 0.0;
-
 		for (int iDim=0;iDim<numberOfDimensions;iDim++){
 			assertEquals(parameterVectorExpected[iDim], parameterVectorActual[iDim],parameterVectorAcceptableDifference);
 		}
 		
-		double objScoreActual = listOfSamplesActual.getObjScore(nPop+1);
+		double objScoreActual = listOfSamplesActual.getObjScore(nPop);
 		double objScoreAcceptableDifference = 0.0;
 		assertEquals(objScoreExpected, objScoreActual,objScoreAcceptableDifference);
 		
 		
 	}
-
-	@Test
-	public void testSetParameterVector() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetParameterVector() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetObjScore() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetObjScore() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testCalcObjScore() {
-		fail("Not yet implemented");
-	}
+//
+//	@Test
+//	public void testSetParameterVector() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetParameterVector() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testSetObjScore() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetObjScore() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testCalcObjScore() {
+//		fail("Not yet implemented");
+//	}
 
 }
