42915ac7efb4b20dcf30136e96592bcd49445bf6
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2013-08-05 09:35:43 +0200

    fixes Jenkins/SomarQube issue about trailing comments

--- src/nl/esciencecenter/diffevo/likelihoodfunctions/LikelihoodFunctionRosenbrockModel.java
+++ src/nl/esciencecenter/diffevo/likelihoodfunctions/LikelihoodFunctionRosenbrockModel.java
@@ -27,13 +27,15 @@ public class LikelihoodFunctionRosenbrockModel implements LikelihoodFunction {
 	}
 	
 	private double calcProbabilityDensity(double[] x){
+		
+		
 		double sum = 0.0;
 		int nPars = x.length;
 		for (int iPar = 0;iPar<nPars-1;iPar++){
 			sum = sum + (1-Math.pow(x[iPar],2) + 100*Math.pow(x[iPar+1]-Math.pow(x[iPar],2), 2));
 		}
-		
-		probabilityDensity = sum; // it's not really a probability density since this is just a benchmark function
+		// it's not really a probability density since this is just a benchmark function
+		probabilityDensity = sum; 
 		return probabilityDensity; 
 	}
 			
