9fbb8b71c3ce8d1d1f1921e9715c20c3a1b3adb8
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2013-08-06 14:54:55 +0200

    added stub for testing the ParSpace class

---
+++ test/src/nl/esciencecenter/diffevo/ParSpaceTest.java
@@ -0,0 +1,64 @@
+package nl.esciencecenter.diffevo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ParSpaceTest {
+
+	@Test
+	public void testParSpace() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumberOfPars() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLowerBound() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUpperBound() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRange() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetParName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDivideIntoIntervalsInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDivideIntoIntervalsIntArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBinBounds() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetResolution() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetnBins() {
+		fail("Not yet implemented");
+	}
+
+}
