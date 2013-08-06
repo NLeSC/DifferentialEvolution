583b841bb32ddd0137f49b925b8122530f9dfd1d
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2013-08-06 14:10:03 +0200

    added Proposals test (not working yet)

---
+++ test/src/nl/esciencecenter/diffevo/ProposalsTest.java
@@ -0,0 +1,29 @@
+package nl.esciencecenter.diffevo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProposalsTest {
+
+	@Test
+	public void testProposals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReflectIfOutOfBounds() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProposals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProposal() {
+		fail("Not yet implemented");
+	}
+
+}
