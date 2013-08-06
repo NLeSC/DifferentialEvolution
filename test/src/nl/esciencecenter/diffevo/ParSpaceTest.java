72f84ed299c880a351c2b82990fc712c90f90569
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2013-08-06 15:55:38 +0200

    added unit test for testing the lower bound of the parameter space

--- test/src/nl/esciencecenter/diffevo/ParSpaceTest.java
+++ test/src/nl/esciencecenter/diffevo/ParSpaceTest.java
@@ -10,7 +10,7 @@ public class ParSpaceTest {
 	private double[] upperBoundsExpected;
 	private String[] parNamesExpected;
 	private ParSpace parSpace;
-	private int nParsExpected = lowerBoundsExpected.length; 
+	private int nParsExpected = 0; 
 	
 	public ParSpaceTest(){
 		
@@ -18,13 +18,15 @@ public class ParSpaceTest {
 		upperBoundsExpected = new double[]{5.12,5.12};
 		parNamesExpected = new String[]{"p1","p2"};
 		parSpace = new ParSpace(lowerBoundsExpected,upperBoundsExpected,parNamesExpected);
+		
+		nParsExpected = lowerBoundsExpected.length;
 
 	}
-	
-	@Test
-	public void testParSpace() {
-		fail("Not yet implemented");
-	}
+//	
+//	@Test
+//	public void testParSpace() {
+//		fail("Not yet implemented");
+//	}
 
 	@Test
 	public void testGetNumberOfPars() {
@@ -40,44 +42,44 @@ public class ParSpaceTest {
 		}
 	}
 
-	@Test
-	public void testGetUpperBound() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetRange() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetParName() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testDivideIntoIntervalsInt() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testDivideIntoIntervalsIntArray() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetBinBounds() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetResolution() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetnBins() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	public void testGetUpperBound() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetRange() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetParName() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testDivideIntoIntervalsInt() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testDivideIntoIntervalsIntArray() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetBinBounds() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetResolution() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetnBins() {
+//		fail("Not yet implemented");
+//	}
 
 }
