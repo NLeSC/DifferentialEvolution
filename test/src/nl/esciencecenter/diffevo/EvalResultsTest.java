61f787b90be91e98c9fe59bb2824f7448e28fdbf
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2013-08-07 12:13:17 +0200

    unit testing stub for EvalResults class

---
+++ test/src/nl/esciencecenter/diffevo/EvalResultsTest.java
@@ -0,0 +1,49 @@
+package nl.esciencecenter.diffevo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EvalResultsTest {
+
+	@Test
+	public void testEvalResults() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEvalResults() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSample() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetParameterVector() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetObjScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSampleCounter() {
+		fail("Not yet implemented");
+	}
+
+}
