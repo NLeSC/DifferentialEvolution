098df5f1cee4056cc03056c777be497bc6e830bf
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2013-08-07 16:57:29 +0200

    added superclass Space

---
+++ src/nl/esciencecenter/diffevo/Space.java
@@ -0,0 +1,63 @@
+/*
+ * Copyrighted 2012-2013 Netherlands eScience Center.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").  
+ * You may not use this file except in compliance with the License. 
+ * For details, see the LICENCE.txt file location in the root directory of this 
+ * distribution or obtain the Apache License at the following location: 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ * 
+ * For the full license, see: LICENCE.txt (located in the root folder of this distribution). 
+ * ---
+ */
+
+package nl.esciencecenter.diffevo;
+
+public class Space {
+
+	private final double[] lowerBounds;
+	private final double[] upperBounds;
+	private final double[] range;	
+	private final String[] dimensionNames;
+	private final int nDimensions;
+
+	public Space(double[] lowerBounds, double[] upperBounds, String[] dimensionNames){
+
+		this.lowerBounds = lowerBounds.clone();
+		this.upperBounds = upperBounds.clone();
+		this.dimensionNames = dimensionNames.clone();
+		this.nDimensions = lowerBounds.length;
+		this.range = new double[nDimensions]; 
+
+		for (int iDim=0;iDim<nDimensions;iDim++){
+			this.range[iDim] = upperBounds[iDim] - lowerBounds[iDim];
+		}
+	}
+
+	public int getNumberOfDimensions() {
+		return nDimensions;
+	}
+
+	public double getLowerBound(int index) {
+		return lowerBounds[index];
+	}
+
+	public double getUpperBound(int index) {
+		return upperBounds[index];
+	}
+
+	public double getRange(int index) {
+		return range[index];
+	}
+
+	public String getDimensionName(int index) {
+		return dimensionNames[index];
+	}
+
+}
