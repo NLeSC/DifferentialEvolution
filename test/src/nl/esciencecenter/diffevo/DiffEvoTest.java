064eaca2ebf2a7be16a617d2945c71820062839f
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2013-08-07 14:05:57 +0200

    added unit testing stub for DiffEvo class

---
+++ test/src/nl/esciencecenter/diffevo/DiffEvoTest.java
@@ -0,0 +1,89 @@
+package nl.esciencecenter.diffevo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DiffEvoTest {
+
+	@Test
+	public void testDiffEvoIntIntParSpaceLikelihoodFunctionFactory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDiffEvoIntIntParSpaceDoubleArrayDoubleArrayDoubleArrayDoubleArrayDoubleArrayArrayModelFactoryLikelihoodFunctionFactory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitializeParents() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProposeOffSpring() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateParentsWithProposals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrintEvalResults() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWriteEvalResultsToTextFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWriteEvalResultsToJSON() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testScatterEvalObj() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testScatterEvalPar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMatrixOfScatterParPar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMatrixOfHeatmapParPar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalcHistogram() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMargHist() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetnPop() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStart() {
+		fail("Not yet implemented");
+	}
+
+}
