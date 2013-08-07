9cd644fffad3218b9d2b9b2cfddae56730c854b8
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2013-08-07 18:04:11 +0200

    added StateSpace extends Space

---
+++ src/nl/esciencecenter/diffevo/StateSpace.java
@@ -0,0 +1,41 @@
+/*
+ * Copyrighted 2012-2013 Netherlands eScience Center.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").  
+ * You may not use this file except in compliance with the License. 
+ * For details, see the LICENCE.txt file location in the root directory of this 
+ * distribution or obtain the Apache License at the following location: 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ * 
+ * For the full license, see: LICENCE.txt (located in the root folder of this distribution). 
+ * ---
+ */
+
+package nl.esciencecenter.diffevo;
+
+public class StateSpace extends Space {
+
+	public StateSpace(double[] lowerBounds, double[] upperBounds, String[] stateNames){
+		
+		super(lowerBounds, upperBounds, stateNames);
+
+	}
+
+	public int getNumberOfStates() {
+		return getNumberOfDimensions();
+	}
+
+	public String getStateName(int index) {
+		return getDimensionName(index);
+	}
+
+
+
+
+}
