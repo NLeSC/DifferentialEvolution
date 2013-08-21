465a3c01ccd2c363fda8671423d4c796a6a3ab0b
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2013-08-21 15:27:52 +0200

    removed the switch design pattern from MainProgram.java

--- src/nl/esciencecenter/diffevo/MainProgram.java
+++ src/nl/esciencecenter/diffevo/MainProgram.java
@@ -38,117 +38,83 @@ public class MainProgram {
 	 */
 	public static void main(String[] args) {
 
-		for (int modelSwitch = 3;modelSwitch<4;modelSwitch++){
 
-			int nGens = 0;
-			int nPop = 0;
-			double[] initState = null;
-			double[] assimilate = null;
-			double[] times = null;
-			double[] forcing = null;
-			ParSpace parSpace = null;
-			StateSpace stateSpace = null;		
-			double[][] obs = null;
-			ModelFactory modelFactory = null;
-			LikelihoodFunctionFactory likelihoodFunctionFactory = null;
-			DiffEvo diffEvo = null;
-			File file = null;
-			
-			switch (modelSwitch){
-			case 1:{
-				//LinearDynamicStateSpaceModel
-				nGens = 500;
-				nPop = 50;
-				file  = new File("data"+File.separator+"lineartank.eas");
-				DataReader reader = new DataReader(file);
-				double[][] data = reader.getData();
+		int nGens = 500;
+		int nPop = 50;
+		File file = new File("data"+File.separator+"lineartank.eas");
+		DataReader reader = new DataReader(file);
+		double[][] data = reader.getData();
 
-				initState = new double[] {30};
-				times = data[0];
-				assimilate = data[1];
-				obs = new double[][]{data[3]};
-				forcing = data[4];
-				{
-					double[] lowerBoundsParSpace = new double[] {10};
-					double[] upperBoundsParSpace = new double[] {500};
-					String[] parNames = new String[] {"resistance"};
-					parSpace = new ParSpace(lowerBoundsParSpace,upperBoundsParSpace,parNames);
-					parSpace.divideIntoIntervals(100);
-				}
-				{
-					double[] lowerBoundsStateSpace = new double[] {0};
-					double[] upperBoundsStateSpace = new double[] {100};
-					String[] stateNames = new String[] {"waterlevel"};
-					stateSpace = new StateSpace(lowerBoundsStateSpace,upperBoundsStateSpace,stateNames);
-				}
-				modelFactory = (ModelFactory) new LinearDynamicStateSpaceModelFactory();
-				likelihoodFunctionFactory = (LikelihoodFunctionFactory) new LikelihoodFunctionSSRFactory();
-				diffEvo = new DiffEvo(nGens, nPop, parSpace, stateSpace, initState, forcing, times, assimilate, obs, modelFactory, likelihoodFunctionFactory);
-				break; 
-			} // case 1
-			default: {
-				System.out.println("You asked for a case that isn\'t there.");
-			}
-			} //switch
+		double[] initState = new double[] {30};
+		double[] times = data[0];
+		double[] assimilate = data[1];
+		double[][] obs = new double[][]{data[3]};
+		double[] forcing = data[4];
+		double[] lowerBoundsParSpace = new double[] {10};
+		double[] upperBoundsParSpace = new double[] {500};
+		String[] parNames = new String[] {"resistance"};
+		ParSpace parSpace = new ParSpace(lowerBoundsParSpace,upperBoundsParSpace,parNames);
+		parSpace.divideIntoIntervals(100);
+		double[] lowerBoundsStateSpace = new double[] {0};
+		double[] upperBoundsStateSpace = new double[] {100};
+		String[] stateNames = new String[] {"waterlevel"};
+		StateSpace stateSpace = new StateSpace(lowerBoundsStateSpace,upperBoundsStateSpace,stateNames);
 
-			// run the optimization:
-			EvalResults evalResults = diffEvo.runOptimization();
-			
-			// do some visualization of the results:
-			DiffEvoVisualization vis = new DiffEvoVisualization(evalResults);
-			vis.matrixOfScatterParPar();
-			vis.matrixOfHeatmapParPar();
-			vis.margHist();
-			vis.scatterEvalObj();
-			for (int iPar=0;iPar<parSpace.getNumberOfPars();iPar++){
-				vis.scatterEvalPar(iPar); 
-			}
-			
-			boolean modelIsDynamic = modelFactory!=null;
-			
-			if (modelIsDynamic){
+		ModelFactory modelFactory = (ModelFactory) new LinearDynamicStateSpaceModelFactory();
+		LikelihoodFunctionFactory likelihoodFunctionFactory = (LikelihoodFunctionFactory) new LikelihoodFunctionSSRFactory();
 
-				XYSeriesCollection data = new XYSeriesCollection();
-				XYSeries series;
-				{
-					series = vis.createSeries("time v. obs",times, obs[0]);
-					data.addSeries(series);
-				}
-				{
-					int iResult[] = evalResults.sampleIdentifiersOfBest();
-					double[][] modelResult = evalResults.getEvalResult(iResult[0]).getModelResult();
-					series = vis.createSeries("time v. prior["+iResult[0]+"]",times, modelResult[0]);
-					data.addSeries(series);
-				}
-				{
-					Random generator = new Random();
-					int iResult = generator.nextInt(evalResults.size());
-					double[][] modelResult = evalResults.getEvalResult(iResult).getModelResult();
-					series = vis.createSeries("time v. randomly selected prior["+iResult+"]", times, modelResult[0]);
-					data.addSeries(series);
-				}
-				
-				Color[] colors = new Color[data.getSeriesCount()];
-				colors[0] = new Color(0,0,255);
-				colors[1] = new Color(0,128,0);
-				colors[2] = new Color(255,128,0);				
-				
-				vis.scatter(data, "states", colors, "time", "state", true, true);
-				
-			}
-			
-		
-			// do some printing to file and standard out:
-			DiffEvoOutputWriters writers = new DiffEvoOutputWriters(evalResults);
-			writers.printEvalResults();
-			writers.printEvalResults();
-			file = new File("out"+File.separator+"evalresults.json");
-			writers.writeEvalResultsToJSON(file);
-			file = new File("out"+File.separator+"evalresults.txt");
-			writers.writeEvalResultsToTextFile(file);
+		DiffEvo diffEvo = new DiffEvo(nGens, nPop, parSpace, stateSpace, initState, forcing, times, 
+				assimilate, obs, modelFactory, likelihoodFunctionFactory);
 
+		// run the optimization:
+		EvalResults evalResults = diffEvo.runOptimization();
 
-		} // int modelSwitch
+		// do some visualization of the results:
+		DiffEvoVisualization vis = new DiffEvoVisualization(evalResults);
+		vis.matrixOfScatterParPar();
+		vis.matrixOfHeatmapParPar();
+		vis.margHist();
+		vis.scatterEvalObj();
+		for (int iPar=0;iPar<parSpace.getNumberOfPars();iPar++){
+			vis.scatterEvalPar(iPar); 
+		}
+
+		XYSeriesCollection dataset = new XYSeriesCollection();
+		XYSeries series;
+		{
+			series = vis.createSeries("time v. obs",times, obs[0]);
+			dataset.addSeries(series);
+		}
+		{
+			int iResult[] = evalResults.sampleIdentifiersOfBest();
+			double[][] modelResult = evalResults.getEvalResult(iResult[0]).getModelResult();
+			series = vis.createSeries("time v. prior["+iResult[0]+"]",times, modelResult[0]);
+			dataset.addSeries(series);
+		}
+		{
+			Random generator = new Random();
+			int iResult = generator.nextInt(evalResults.size());
+			double[][] modelResult = evalResults.getEvalResult(iResult).getModelResult();
+			series = vis.createSeries("time v. randomly selected prior["+iResult+"]", times, modelResult[0]);
+			dataset.addSeries(series);
+		}
+
+		Color[] colors = new Color[dataset.getSeriesCount()];
+		colors[0] = new Color(0,0,255);
+		colors[1] = new Color(0,128,0);
+		colors[2] = new Color(255,128,0);				
+
+		vis.scatter(dataset, "states", colors, "time", "state", true, true);
+
+		// do some printing to file and standard out:
+		DiffEvoOutputWriters writers = new DiffEvoOutputWriters(evalResults);
+		writers.printEvalResults();
+		writers.printEvalResults();
+		file = new File("out"+File.separator+"evalresults.json");
+		writers.writeEvalResultsToJSON(file);
+		file = new File("out"+File.separator+"evalresults.txt");
+		writers.writeEvalResultsToTextFile(file);
+
 	} // main
 } //Test
 
